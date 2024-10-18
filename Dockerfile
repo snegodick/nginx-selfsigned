dbf8fd51ae9fd94af1dfb95ffc1382482adf64ff
Snegodick <ne@ovrnet.ru>
2024-10-18 12:47:14 +0300

    certs method fixed

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
+
 FROM debian:bullseye
 RUN apt update
 RUN apt install -y nginx-full openssl && apt clean && rm -rf /var/lib/apt/lists/*
 #apt install procps
-RUN openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout /etc/nginx/server.key -out /etc/nginx/server.crt -subj "/C=US/ST=New Sweden/L=Stockholm /O= /OU= /CN= /emailAddress= " 
+#RUN openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout /etc/nginx/server.key -out /etc/nginx/server.crt -subj "/C=US/ST=New Sweden/L=Stockholm /O= /OU= /CN= /emailAddress= " 
 RUN update-rc.d nginx remove
 COPY default /etc/nginx/sites-enabled/
 COPY entry.sh /opt/
--- entry.sh
+++ entry.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+CERT_KEY="/etc/nginx/server.key"
+CERT_CRT="/etc/nginx/server.crt"
+
+# Генерация сертификата, если он не существует
+if [ ! -f "$CERT_KEY" ] || [ ! -f "$CERT_CRT" ]; then
+    echo "Generating self-signed certificate..."
+    openssl req -x509 -nodes -days 3650 -newkey rsa:2048 \
+    -keyout "$CERT_KEY" -out "$CERT_CRT" \
+    -subj "/C=US/ST=New Sweden/L=Stockholm /O= /OU= /CN= /emailAddress= "
+else
+    echo "Using existing certificates."
+fi
+
 echo "Remote URL: $NGX_FORWARD_TO"
 sed -i -e "s|NGX_FORWARD_TO|$NGX_FORWARD_TO|g" /etc/nginx/sites-enabled/default
 exec nginx -g "daemon off;"
+#nginx -g 'daemon off;'
